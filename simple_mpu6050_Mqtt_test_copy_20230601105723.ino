b2778e21c335acd271627da6414a0a9b654d2d74
Kueschall Carl <c.kueschall@oth-aw.de>
2023-06-06 19:15:26 +0200

    Create simple_mpu6050_Mqtt_test_copy_20230601105723.ino

---
+++ simple_mpu6050_Mqtt_test_copy_20230601105723.ino
@@ -0,0 +1,144 @@
+
+#include <Adafruit_MPU6050.h>
+#include <Adafruit_Sensor.h>
+#include <Wire.h>
+#include <WiFi.h>
+#include <PubSubClient.h>
+
+WiFiClient espClient;
+PubSubClient mqttClient(espClient);
+Adafruit_MPU6050 mpu; 
+
+
+const char* ssid = "MaraudersMap";
+const char* password = "Page394%";
+//bei public broker = "broker" teiweise
+const char* mqttBroker = "127.0.0.0";
+const int mqttPort = 1883;
+const char* mqttTopic = "gyro_x";
+
+void onMqttMessageReceived(char* topic, byte* payload, unsigned int length) {
+  // Handle MQTT message received
+  // Convert payload to a string
+  String message;
+  for (unsigned int i = 0; i < length; i++) {
+    message += (char)payload[i];
+  }
+
+  // Display the received message in the console
+  Serial.print("Received message on topic: ");
+  Serial.print(topic);
+  Serial.print(", payload: ");
+  Serial.println(message);
+}
+
+
+void connectToWifi() {
+  WiFi.begin(ssid, password);
+  Serial.print("Connecting to Wi-Fi...");
+  while (WiFi.status() != WL_CONNECTED) {
+    delay(100);
+  }
+  Serial.println("connected!");
+
+  Serial.print("IP address: ");
+  Serial.println(WiFi.localIP());
+}
+
+
+
+void setupMqtt() {
+  mqttClient.setServer(mqttBroker, mqttPort);
+  mqttClient.setCallback(onMqttMessageReceived);
+
+  Serial.print("Connecting to MQTT broker...");
+  while (!mqttClient.connected()) {
+    if (mqttClient.connect("ESP32Client")) {
+      Serial.println("connected!");
+      mqttClient.subscribe(mqttTopic);
+    } else {
+      Serial.print("failed, retrying in 5 seconds...");
+      delay(5000);
+    }
+  }
+}
+
+
+
+
+void setup() {
+  //sets the pins for the MPU6050 chip IMPORTANT!!!!!!!!!!!!!!!!!!!!!!!!!
+  Wire.begin(0,4);
+  
+  Serial.begin(115200);
+  while (!Serial)
+    delay(10);
+
+  Serial.println("Adafruit MPU6050 test!");
+
+  if (!mpu.begin()) {
+    Serial.println("Failed to find MPU6050 chip");
+    while (1) {
+      delay(10);
+    }
+  } 
+  Serial.println("MPU6050 Found!");
+
+  mpu.setAccelerometerRange(MPU6050_RANGE_8_G);
+  Serial.print("Accelerometer range set to: +-8G");
+
+  mpu.setGyroRange(MPU6050_RANGE_500_DEG);
+  Serial.print("Gyro range set to: +- 1000 deg/s");
+
+  mpu.setFilterBandwidth(MPU6050_BAND_21_HZ);
+  Serial.print("Filter bandwidth set to: 44hz");
+  Serial.println("");
+  delay(10000);
+
+  // Connect to Wi-Fi
+  connectToWifi();
+
+  // Setup MQTT
+  setupMqtt();
+}
+
+
+
+
+void loop() {
+   /* Get new sensor events with the readings */
+  sensors_event_t a, g, temp;
+  mpu.getEvent(&a, &g, &temp);
+
+  // Publish the g.gyro.x value
+  String gyroXValue = String(g.gyro.x);
+  mqttClient.publish(mqttTopic, gyroXValue.c_str());
+
+  /* Print out the values */
+  Serial.print("Acceleration X: ");
+  Serial.print(a.acceleration.x);
+  Serial.print(", Y: ");
+  Serial.print(a.acceleration.y);
+  Serial.print(", Z: ");
+  Serial.print(a.acceleration.z);
+  Serial.println(" m/s^2");
+
+  Serial.print("Rotation X: ");
+  Serial.print(g.gyro.x);
+  Serial.print(", Y: ");
+  Serial.print(g.gyro.y);
+  Serial.print(", Z: ");
+  Serial.print(g.gyro.z);
+  Serial.println(" rad/s");
+
+  Serial.print("Temperature: ");
+  Serial.print(temp.temperature);
+  Serial.println(" degC");
+
+  Serial.println("");
+  delay(500);
+
+  // Process MQTT messages
+  mqttClient.loop();
+}
+
